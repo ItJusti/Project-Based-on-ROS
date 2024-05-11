3e7a1aaa37a90dd4389246b3fcd05f0a085ea212
ItJusti <firmanfitrahramadhan38@gmail.com>
2024-05-11 17:32:30 +0700

    File

---
+++ catkin_ws_tugas/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/firman/catkin_ws_tugas/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/firman/catkin_ws_tugas/devel;/home/firman/catkin_ws/devel;/home/firman/catkin_ws_robotik/devel;/opt/ros/noetic'.split(';') if '/home/firman/catkin_ws_tugas/devel;/home/firman/catkin_ws/devel;/home/firman/catkin_ws_robotik/devel;/opt/ros/noetic' != '' else []
---
+++ catkin_ws_tugas/build/robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/firman/catkin_ws_tugas/src/robot
+# Build directory: /home/firman/catkin_ws_tugas/build/robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
